e856b35d3de4f2fa994c769ab3e9ed9cec70c0ca
twinh <twinhuang@qq.com>
2017-10-26 21:50:09 +0800

    更正url，因直接访问会出错

--- resources/views/wechat-poi/adminShopsEdit.php
+++ resources/views/wechat-poi/adminShopsEdit.php
@@ -3,7 +3,8 @@
   // 图片上传后同步到微信中
   $('.js-image-upload').on('fileuploaded', function (event, data) {
     $.ajax({
-      url: $.url('admin/wechat-medias/create'),
+      url: $.url('admin/wechat-medias/create.json'),
+      dataType: 'json',
       type: 'post',
       data: {
         url: data.response.url
